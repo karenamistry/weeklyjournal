89319721cf72f98fe38eb296ae4bd9b3edcd6196
karenamistry <43101605+karenamistry@users.noreply.github.com>
2019-12-03 14:27:47 -0500

    Add files via upload

---
+++ myScript.js
@@ -0,0 +1,6 @@
+function changePicture(){
+    document.getElementById("animateStrum").src="images/animation-strum.gif";
+}
+function stillPicture(){
+    document.getElementById("animateStrum").src="images/animation-strum-still.jpg";
+}
